37e07c6008adf08c7a660b685ae53aa3389cd1af
Sarah Banashek <onplanetoff@darwin.attlocal.net>
2020-12-21 14:47:27 -0800

    adding ChartDisplayComponent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { Chart } from "react-google-charts";
-
+import { BarChart, Bar, Cell, XAxis, YAxis, Tooltip } from 'recharts';
 
 function App() {
   const [url, setUrl] = useState('https://api.ratesapi.io/api/latest');
@@ -15,8 +14,8 @@ function App() {
       const json = await response.json();
 
       const rates = Object.entries(json.rates).map(kv => {
-        return [kv[0], kv[1], currencyNames[kv[0]]]
-      })
+        return {abbreviation: kv[0], rate: kv[1]};
+      }).sort((a, b) => a.abbreviation < b.abbreviation ? -1 : 1);
 
       const newData = {
         base: json.base,
@@ -26,22 +25,84 @@ function App() {
       setData(newData);
       setIsLoading(false);
     }
+
     fetchData();
   }, [url]);
 
-  console.log(data.rates);
+  // console.log(data);
 
   return (
     <div className="App">
       {isLoading ? <p>Loading</p> : <div id="base-currency">{data.base}</div>}
-      <div id="rates">
-        {data.rates}
-      </div>
+      {isLoading ? <p>Loading</p> : <ChartDisplayComponent value={{data, isLoading}} />}
     </div>
   );
 }
 
+function CustomizedAxisTick({x, y, stroke, payload}) {
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text x={0} y={0} dy={16} fontSize="10px" textAnchor="end" fill="#666" transform="rotate(-35)">{payload.value}</text>
+    </g>
+  );
+}
 
+function getCurrencyName(label) {
+  return currencyNames[label];
+}
+
+function CustomTooltip({ active, payload, label }) {
+  if (active) {
+    return (
+      <div className="custom-tooltip">
+        <p className="label">{getCurrencyName(label)}</p>
+        <p className="rate">{`1 ${payload[0].unit} = ${payload[0].value} ${label}s`}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+function getFillColor(rates) {
+  if (rates >= 10000) {
+    return "#023858";
+  } else if (rates >= 1000) {
+    return "#045a8d";
+  } else if (rates >= 500) {
+    return "#0570b0";
+  } else if (rates >= 100) {
+    return "#3690c0";
+  } else if (rates >= 50) {
+    return "#74a9cf";
+  } else if (rates >= 10) {
+    return "#a6bddb";
+  } else if (rates >= 2) {
+    return "#d0d1e6";
+  } else if (rates >= 1) {
+    return "#ece7f2";
+  } else {
+    return "#fff7fb";
+  }
+}
+
+function ChartDisplayComponent(props) {
+  const rates = props.value.data.rates;
+  return (
+    <BarChart width={1000} height={400} margin={{bottom: 20}} data={rates}>
+      <XAxis dataKey="abbreviation" interval={0} minTickGap={20} tick={<CustomizedAxisTick />} />
+      <YAxis dataKey="rate" domain={[0.01, 'auto']} scale="log"/>
+      <Tooltip content={<CustomTooltip />} />
+      <Bar dataKey="rate" unit={props.value.data.base} >
+        {
+          rates.map((entry, index) => (
+            <Cell key={index} fill={getFillColor(entry.rate)} />
+          ))
+        }
+      </Bar>
+  </BarChart>
+  );
+}
 
 const currencyNames = {
   AUD: 'Australian Dollar',
