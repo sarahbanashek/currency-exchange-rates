f393e9e7fec849054abaa6bd881d86ca2e39a2b9
Sarah Banashek <sarahmbanashek@gmail.com>
2021-02-02 16:25:25 -0800

    updating styling & functionality of chart customization

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import { BarChart, Bar, Cell, XAxis, YAxis, Tooltip, Label, ResponsiveContainer } from 'recharts';
+const DEFAULT_CURRENCY = 'EUR';
+const DEFAULT_URL = 'https://api.ratesapi.io/api/latest';
 
 function App() {
-  const [url, setUrl] = useState('https://api.ratesapi.io/api/latest');
+  const [url, setUrl] = useState(DEFAULT_URL);
   const [data, setData] = useState({});
+  const [hasLoadedInitialData, setHasLoadedInitialData] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [chartDate, setChartDate] = useState();
-  const [baseCurrency, setBaseCurrency] = useState();
+  const [baseCurrency, setBaseCurrency] = useState(DEFAULT_CURRENCY);
+  const [symbols, setSymbols] = useState();
 
 
-function updateUrl(symbols) {
-  if (chartDate && baseCurrency && symbols) {
-    setUrl(`https://api.ratesapi.io/api/${chartDate}?base=${baseCurrency}&symbols=${symbols}`);
-  } else if (chartDate && baseCurrency) {
-    setUrl(`https://api.ratesapi.io/api/${chartDate}?base=${baseCurrency}`);
-  } else if (chartDate && symbols) {
-    setUrl(`https://api.ratesapi.io/api/${chartDate}?symbols=${symbols}`);
-  } else if (chartDate) {
-    setUrl(`https://api.ratesapi.io/api/${chartDate}`);
-  } else if (baseCurrency && symbols) {
-    setUrl(`https://api.ratesapi.io/api/latest?base=${baseCurrency}&symbols=${symbols}`);
-  } else if (baseCurrency) {
-    setUrl(`https://api.ratesapi.io/api/latest?base=${baseCurrency}`);
-  } else if (symbols) {
-    setUrl(`https://api.ratesapi.io/api/latest?symbols=${symbols}`);
+  function updateSymbols(symbolsString) {
+    setSymbols(symbolsString);
+    chartDate
+      ? setUrl(`https://api.ratesapi.io/api/${chartDate}?base=${baseCurrency}&symbols=${symbolsString}`)
+      : setUrl(`https://api.ratesapi.io/api/latest?base=${baseCurrency}&symbols=${symbolsString}`)
   }
-}
 
-function updateChartDate(date) {
-  setChartDate(date);
-}
+  function updateChartDate(date) {
+    setChartDate(date);
+    setUrl(`https://api.ratesapi.io/api/${date}`);
+    setBaseCurrency(DEFAULT_CURRENCY);
+    setSymbols(null);
+  }
 
-function updateBaseCurrency(newBase) {
-  setBaseCurrency(newBase);
-}
+  function updateBaseCurrency(newBase) {
+    setBaseCurrency(newBase);
+
+    if (chartDate && symbols) {
+      setUrl(`https://api.ratesapi.io/api/${chartDate}?base=${newBase}&symbols=${symbols}`);
+    } else if (chartDate) {
+      setUrl(`https://api.ratesapi.io/api/${chartDate}?base=${newBase}`);
+    } else if (symbols) {
+      setUrl(`https://api.ratesapi.io/api/latest?base=${newBase}&symbols=${symbols}`);
+    } else {
+      setUrl(`https://api.ratesapi.io/api/latest?base=${newBase}`);
+    }
+  }
+
+  function resetUrl() {
+    setUrl('https://api.ratesapi.io/api/latest');
+    setBaseCurrency(DEFAULT_CURRENCY);
+    setChartDate(null);
+    setSymbols(null);
+  }
 
   useEffect(() => {
-    console.log('in effect')
     async function fetchData() {
       setIsLoading(true);
       const response = await fetch(url);
@@ -55,98 +67,115 @@ function updateBaseCurrency(newBase) {
 
       setData(newData);
       setIsLoading(false);
+      if (!hasLoadedInitialData) setHasLoadedInitialData(true);
     }
 
     fetchData();
-  }, [url]);
+  }, [url, hasLoadedInitialData]);
 
-  return (
-    <div className="App">
-      <div id="title">World Currencies and Exchange Rates</div>
-      {isLoading ? <p>Loading</p> : <SelectDate {...{updateUrl, updateChartDate}} />}
-      {isLoading ? <p>Loading</p> : <ChartDisplayComponent {...{data}} />}
-      {isLoading ? <p>Loading</p> : <SelectBaseAndSymbols {...{updateUrl, data, updateBaseCurrency}} />}
-    </div>
-  );
+  if (!hasLoadedInitialData) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div className="App">
+        <div id="title">World Currencies and Exchange Rates</div>
+        <ChartDisplayComponent {...{data}} />
+        <div id="form-container">
+          <SelectDate {...{updateChartDate, isLoading, data}} />
+          <SelectBaseAndSymbols {...{updateSymbols, data, baseCurrency, updateBaseCurrency, isLoading, resetUrl}} />
+        </div>
+      </div>
+    );
+  }
 }
 
-function SelectDate({updateUrl, updateChartDate}) {
+function SelectDate({updateChartDate, isLoading, data}) {
   const today = new Date().toISOString().split("T")[0];
-  function changeDate() {
-    updateChartDate(document.getElementById('date').value);
+  function changeDate(e) {
+    updateChartDate(e.target.value);
   }
-  function submit() {
-    updateUrl();
-  }
+
   return (
     <div id="select-date">
       <label htmlFor="date">Select a date (from January 4th, 1999)</label>
-      <input type="date" id="date"
-          min="1999-01-04" 
-          max={today}
-          pattern="\d{4}-\d{2}-\d{2}"
-          placeholder="yyyy-mm-dd"
-          onChange={() => changeDate()}></input>
-      <button type="button" id="update-date-button" onClick={() => submit()} >Submit</button>
+      {isLoading
+        ? <input type="date" id="date"
+            value={data.date}
+            placeholder="yyyy-mm-dd"
+            disabled />
+        : <input type="date" id="date"
+            value={data.date}
+            min="1999-01-04" 
+            max={today}
+            pattern="\d{4}-\d{2}-\d{2}"
+            placeholder="yyyy-mm-dd"
+            onChange={(e) => changeDate(e)}/>
+            }
     </div>
   );
 }
 
-function SelectBaseAndSymbols({updateUrl, data, updateBaseCurrency}) {
-  let base, symbols, isEURListed;
-  const availableSymbols = data.rates.map(rateObj => {
-    if (rateObj.abbreviation === 'EUR') isEURListed = true;
-    return rateObj.abbreviation;
-  });
-  function selectBase() {
-    base = document.getElementById('base-currency').value;
-    updateBaseCurrency(base);
+function SelectBaseAndSymbols({updateSymbols, data, baseCurrency, updateBaseCurrency, isLoading, resetUrl}) {
+  const [currencies, setCurrencies] = React.useState(new Map(data.rates.map(rateObj => rateObj.abbreviation).map(s => [s, false])));
+
+  useEffect(() => {
+    setCurrencies(new Map(data.rates.map(rateObj => rateObj.abbreviation).map(s => [s, false])));
+  }, [data.date])
+  
+  const toggleCurrency = (currencyName) => {
+    const isCurrencySelected = currencies.get(currencyName);
+    setCurrencies((currentCurrencies) => new Map(currentCurrencies).set(currencyName, !isCurrencySelected));
   }
-  function selectSymbols(symbol) {
-    symbols
-      ? symbols += `,${document.getElementById('checkbox-' + symbol).value}`
-      : symbols = document.getElementById('checkbox-' + symbol).value;
+  
+  function selectBase(e) {
+    updateBaseCurrency(e.target.value);
   }
+
   function submit() {
-    if (symbols) {
-      updateUrl(symbols);
-    } else {
-      updateUrl();
-    }
+    const symbolsToSubmit = [...currencies.entries()].filter(([_, v]) => v).map(([k, _]) => k);
+    const symbolsString = symbolsToSubmit.join(',');
+    updateSymbols(symbolsString);
   }
-  console.log(data.rates);
-  console.log(data.base);
+
+  function resetSelection() {
+    setCurrencies(new Map([...currencies.keys()].map(s => [s, false])));
+    resetUrl();
+  }
 
   return (
     <div id="customize-chart">
-      <label htmlFor="base-currency">Select base currency</label>
-      <select name="base-currency" id="base-currency" defaultValue={data.base} onChange={() => selectBase()}>
-        {!isEURListed
-          ? (<option key='EUR' value='EUR' >EUR</option>)
-          : null
-        }
-        {availableSymbols.map(currency => 
-          <option key={currency} value={currency} >{currency}</option>
-        )}
-      </select>
+      <label htmlFor="base-currency">Select a base currency</label>
+      {isLoading
+        ? <select name="base-currency" id="base-currency" value={baseCurrency} disabled>
+            <option key={baseCurrency} value={baseCurrency} >{baseCurrency}</option>
+          </select>
+        : <select name="base-currency" id="base-currency" value={baseCurrency} onChange={(e) => selectBase(e)}>
+            <option key={baseCurrency} value={baseCurrency} >{baseCurrency}</option>
+            {[...currencies.keys()].map(currency => 
+              <option key={currency} value={currency} >{currency}</option>
+            )}
+          </select>
+      }
       <div id="checkboxes-label">Choose which currencies are displayed:</div>
+      {isLoading ? "loading" :
       <div id="checkboxes">
-        {availableSymbols.map(currency => (
-          <div id={currency} className="checkbox-div">
-            <label title={getCurrencyName(currency)}>
-              <input type="checkbox" hidden 
-                id={'checkbox-' + currency} 
-                key={currency}
-                value={currency} 
-                name={currency} 
-                title={getCurrencyName(currency)} 
-                onChange={() => {selectSymbols(currency)}}/>
-              <span>{currency}</span>
-            </label>
-          </div>
-        ))}
-      </div>
-      <button type="button" id="update-url-button" onClick={() => submit()} >Submit</button>
+        {[...currencies.entries()].map(([currency, isSelected]) => {
+          return (
+          <button key={currency} title={getCurrencyName(currency)} onClick={() => toggleCurrency(currency)} className={'currency-button' + (isSelected ? ' selected-currency' : '')}>
+            <span>{currency}</span>    
+          </button>
+        )})}
+      </div>}
+      {isLoading 
+        ? (<div id="button-container">
+            <button type="button" id="update-url-button" disabled >Submit</button>
+            <button type="button" id="reset-url-button" disabled >Reset Chart</button>
+          </div>)
+        : (<div id="button-container">
+            <button type="button" id="update-url-button" onClick={() => submit()} >Submit</button>
+            <button type="button" id="reset-url-button" onClick={() => resetSelection()} >Reset Chart</button>
+          </div>)
+      }
     </div>
   );
 }
@@ -154,7 +183,7 @@ function SelectBaseAndSymbols({updateUrl, data, updateBaseCurrency}) {
 function CustomizedYAxisTick({x, y, _, payload}) {
   return (
     <g transform={`translate(${x},${y})`}>
-      <text x={0} y={5} textAnchor="end" fill="#404040">{Math.round(payload.value * 100)/100}</text>
+      <text x={0} y={5} textAnchor="end" fill="#404040">{Math.round(payload.value * 100000)/100000}</text>
     </g>
   );
 }
@@ -176,7 +205,7 @@ function CustomTooltip({ active, payload, label }) {
     return (
       <div className="custom-tooltip">
         <p className="label">{getCurrencyName(label)}</p>
-        <p className="rate">{`1 ${payload[0].unit} = ${payload[0].value} ${label}s`}</p>
+        <p className="rate">{`1 ${payload[0].unit} = ${payload[0].value} ${label}`}</p>
       </div>
     );
   }
@@ -233,19 +262,19 @@ function ChartDisplayComponent({data}) {
       <div id="chart-title">
         Exchange Rates for the {getCurrencyName(base)} on {titleDate}
       </div>
-      <ResponsiveContainer class="chart-container" width="95%" height={400} >
+      <ResponsiveContainer className="chart-container" width="95%" height={400} >
         <BarChart margin={{top: 50, bottom: 50, left: 20}} data={rates}>
           <XAxis dataKey="abbreviation" interval={0} minTickGap={20} tick={<CustomizedXAxisTick />} >
-            <Label value="World Currencies" offset={10} position="bottom" fill="#404040" />
+            <Label value="World Currencies" offset={20} position="bottom" fill="#404040" />
           </XAxis>
           <YAxis dataKey="rate" domain={[0.01, 'auto']} scale="log" tick={<CustomizedYAxisTick />} >
-            <Label value="Exchange Rate" angle={-90} offset={10} position="left" fill="#404040" />
+            <Label value="Exchange Rate" angle={-90} offset={15} position="left" fill="#404040" />
           </YAxis>
-          <Tooltip content={<CustomTooltip />} />
+          <Tooltip content={<CustomTooltip />} cursor={{ fill: '#EFEFEF' }} />
           <Bar dataKey="rate" unit={base} >
             {
               rates.map((entry, index) => (
-                <Cell key={index} fill={getFillColor(entry.rate)} />
+                <Cell className="bar" key={index} fill={getFillColor(entry.rate)} />
               ))
             }
           </Bar>
